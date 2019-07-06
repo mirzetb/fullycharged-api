b9ba443183e8a0505435fc7309bad2b54f241fd0
Muhammad Haroon Arshad <haroon_ars@hotmail.com>
2019-07-06 03:17:54 +0200

    - Added Routes for addLocation

---
+++ src/routes/chargingLocation.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const chargingLocationController = require('../controllers/chargingLocation');
+
+router.post('/addlocation', chargingLocationController.addLocation());
+
+module.exports = router;
